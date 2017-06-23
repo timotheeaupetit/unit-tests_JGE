8587560331e36f4deb4d308fb856eca4e4f77cb8
Timothee AUPETIT <timothee.aupetit@gmail.com>
2017-06-23 17:03:06 +0200

    added modifyUser method

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,5 +1,16 @@
-class UserController {
+const _ = require('underscore');
 
+class UserController {
+    constructor(registerService) {
+        this._registerService = registerService;
+    }
+
+    modifyUser(req, res) {
+        if (!this._fName
+            ||!this.lName) {
+            "A user must have a firstname and a lastname"
+        }
+    }
 }
 
 module.exports = UserController;
--- test/controllers/UserController.js
+++ test/controllers/UserController.js
@@ -2,5 +2,9 @@ const expect = require('expect');
 const UserController = require('../../app/controllers/IndexCtrl');
 
 describe("UserController", () => {
-
+    describe("#modifyUser", () => {
+        it("Test champs vides : User/user", () => {
+            
+        })
+    });
 });
